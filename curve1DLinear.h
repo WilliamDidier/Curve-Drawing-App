51f3e4ba364e48529d2e7694b36cf0fd6373871c
M1SIAM INP Didier <TMP-Didier@F215-10.e-im2ag.ujf-grenoble.fr>
2017-11-20 15:04:30 +0100

    Indentation code curve 1D Linear

--- curve1DLinear.h
+++ curve1DLinear.h
@@ -7,15 +7,15 @@ class Curve1DLinear : public Curve1D {
  public:
  Curve1DLinear(const QString &name) : Curve1D(name) {}
  Curve1DLinear(Curve1D *curve,const QString &name) : Curve1D(curve,name) {}
-  
+
   QPainterPath path(float xmin,float xmax) {
     QPainterPath p;
 
-    // empty test 
-    if(empty()) 
+    // empty test
+    if(empty())
       return p;
 
-    // left part 
+    // left part
     if(xmin<_points[0][0]) {
       p.moveTo(xmin,_points[0][1]);
       p.lineTo(_points[0][0],_points[0][1]);
@@ -23,12 +23,12 @@ class Curve1DLinear : public Curve1D {
       p.moveTo(_points[0][0],_points[0][1]);
     }
 
-    // draw function 
+    // draw function
     for(unsigned int i=1;i<nbPts();++i) {
       p.lineTo(_points[i][0],_points[i][1]);
     }
 
-    // right part 
+    // right part
     if(xmax>_points[nbPts()-1][0]) {
       p.lineTo(xmax,_points[nbPts()-1][1]);
     }
@@ -37,7 +37,7 @@ class Curve1DLinear : public Curve1D {
   }
 
   float evalAt(float x) {
-    // special cases 
+    // special cases
     if(empty()) return 0.0f;
     if(x<=_points[0][0]) return _points[0][1];
     if(x>=_points[nbPts()-1][0]) return _points[nbPts()-1][1];
@@ -45,8 +45,8 @@ class Curve1DLinear : public Curve1D {
     // linear interp
     for(unsigned int i=0;i<nbPts()-1;++i) {
       if(_points[i+1][0]>=x) {
-	return _points[i][1]+(_points[i+1][1]-_points[i][1])*
-	  ((x-_points[i][0])/(_points[i+1][0]-_points[i][0]));
+	       return _points[i][1]+(_points[i+1][1]-_points[i][1])*
+	        ((x-_points[i][0])/(_points[i+1][0]-_points[i][0]));
       }
     }
     return _points[0][1];
